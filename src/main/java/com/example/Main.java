58a73304020829b79771b2ecd5d04d3ac856e1ce
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 08:46:17 +0200

    socket server

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,15 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket s1 = new ServerSocket(3000);
+        System.out.println("Server avviato!");
+
+        Socket s = s1.accept();
+        System.out.println("Un client si è collegato!");
     }
 }
